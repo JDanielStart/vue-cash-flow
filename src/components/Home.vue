cfd117353ea79d3cbfd65e1e1e57e3185aa2ef3d
JDaniel <daniel@JDStart.onmicrosoft.com>
2024-01-31 19:46:32 +0000

    The "layout" component was built using "movements"

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,17 +4,22 @@
         <template #header>
             <Header/>
         </template>
+        <template #resume>
+            <Resume/>
+        </template>
     </Layout>
 </template>
 
 <script>
     import Layout from './layout.vue';
     import Header from './Header.vue';
+    import Resume from "./Resume/index.vue"
 
     export default {
         components: {
             Layout,
-            Header
+            Header,
+            Resume
 }
     }
 </script>
